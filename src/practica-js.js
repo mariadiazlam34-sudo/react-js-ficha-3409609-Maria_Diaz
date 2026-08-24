9cbd25d3c398108215e1237e7aa884262860f818
mariadiazlam34-sudo <maria.diazlam34@gmail.com>
2026-08-24 09:51:31 -0500

    Reto 7: some(), every() y reduce()

--- src/practica-js.js
+++ src/practica-js.js
@@ -11,20 +11,20 @@ const productos = [
     { id: 9, nombre: 'Botas', precio: 220000, categoria: 'Calzado', stock: 2 },
     { id: 10, nombre: 'Bufanda', precio: 45000, categoria: 'Accesorios', stock: 7 }
 ];
-// RETO 6
-const disponible = productos.filter(producto => producto.precio > 100000);
-const productosRango = productos.filter(producto => 
-    producto.precio >= 50000 && producto.precio <= 200000
+// RETO 7
+const hayAgotados = productos.some(producto => producto.stock === 0);
+console.log("¿Hay productos agotados?", hayAgotados);
+const hayProductoMillonario = productos.some(producto => producto.precio > 1000000);
+console.log("¿Hay productos con precio mayor a $1.000.000?", hayProductoMillonario);
+const preciosValidos = productos.every(producto => producto.precio > 0);
+console.log("¿Todos tienen precio mayor que cero?", preciosValidos);
+const stockValido = productos.every(producto => producto.stock >= 0);
+console.log("¿Todos tienen stock válido?", stockValido);
+const valorInventario = productos.reduce(
+    (total, producto) => total + producto.precio * producto.stock,
+    0
 );
-const productosRopa = productos.filter(producto => 
-    producto.categoria === 'Ropa'
-);
-const producto5 = productos.find(producto => producto.id === 5);
-
-const buscarProducto = id => {
-    return productos.find(producto => producto.id === id);
-};
-console.log(disponible),console.log(productosRango),console.log(productosRopa),console.log(producto5),console.log(buscarProducto(5));
+console.log("Valor total del inventario:", valorInventario);
 
 
 
