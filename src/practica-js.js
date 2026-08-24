a3ffa3b9e20ee281318da122aec60041d1da4b32
mariadiazlam34-sudo <maria.diazlam34@gmail.com>
2026-08-24 09:07:38 -0500

    Reto 3: arreglos y objetos

--- src/practica-js.js
+++ src/practica-js.js
@@ -1,14 +1,16 @@
-//RETO 2
-const cuadrado = (numero) => {
-    return numero * numero;
-};
-const MayorEdad = Edad => Edad >= 18;
+//RETO 3
+const productos = [
+    { id: 1, nombre: 'Camiseta', precio: 50000, categoria: 'Ropa', stock: 5 },
+    { id: 2, nombre: 'Pantalon', precio: 90000, categoria: 'Ropa', stock: 0 },
+    { id: 3, nombre: 'Zapatos', precio: 150000, categoria: 'Calzado', stock: 3 },
+    { id: 4, nombre: 'Chaqueta', precio: 180000, categoria: 'Ropa', stock: 4 },
+    { id: 5, nombre: 'Gorra', precio: 35000, categoria: 'Accesorios', stock: 8 },
+    { id: 6, nombre: 'Bolso', precio: 120000, categoria: 'Accesorios', stock: 0 },
+    { id: 7, nombre: 'Vestido', precio: 140000, categoria: 'Ropa', stock: 6 },
+    { id: 8, nombre: 'Sandalias', precio: 75000, categoria: 'Calzado', stock: 10 },
+    { id: 9, nombre: 'Botas', precio: 220000, categoria: 'Calzado', stock: 2 },
+    { id: 10, nombre: 'Bufanda', precio: 45000, categoria: 'Accesorios', stock: 7 }
+];
 
-const NombreCompleto = (Nombre,apellido) => {
-return Nombre + apellido;
-};
-console.log(cuadrado(2));
-console.log(MayorEdad(19));
-console.log(NombreCompleto('maria','diaz'));
 
  
