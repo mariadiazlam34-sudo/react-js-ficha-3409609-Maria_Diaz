765cdd8aaa294d6aa7f08794335eac67f354a419
mariadiazlam34-sudo <maria.diazlam34@gmail.com>
2026-08-24 09:04:54 -0500

    Reto 2: funciones flecha

--- src/practica-js.js
+++ src/practica-js.js
@@ -1,22 +1,14 @@
-//SUMA
-function RealizarSuma(a, b) {const Suma= a + b;return Suma;}
-const resultado_suma = RealizarSuma(5, 2);
-console.log(resultado_suma);
-//RESTA
-function RealizarResta(a, b) {const Resta= a - b;return Resta;}
-const resultado_resta = RealizarResta(5, 2);
-console.log(resultado_resta);
-//MULTIPLICACION
-function Realizar_Multiplicacion(a, b) {const Multi= a * b;return Multi;}
-const resultado_multi = Realizar_Multiplicacion(5, 2);
-console.log(resultado_multi);
-//DIVISION
-function RealizarDivision(a, b) {const Division= a / b;return Division;}
-const resultado_Division = RealizarDivision(5, 2);
-console.log(resultado_Division);
-// PROMEDIO
-function calcularPromedio(nota1, nota2,nota3) {const total = (nota1 + nota2 + nota3)/3;return total;}
-const resultado = calcularPromedio(1,2,3);
-console.log(resultado)
-// Console log muestra un valor o mensaje en la consula y Return devuelve un valor para poder usarlo despues
+//RETO 2
+const cuadrado = (numero) => {
+    return numero * numero;
+};
+const MayorEdad = Edad => Edad >= 18;
+
+const NombreCompleto = (Nombre,apellido) => {
+return Nombre + apellido;
+};
+console.log(cuadrado(2));
+console.log(MayorEdad(19));
+console.log(NombreCompleto('maria','diaz'));
+
  
